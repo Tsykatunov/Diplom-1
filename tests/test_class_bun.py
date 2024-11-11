4d2c750cb12badc427c704ca3ca89d49e5000a8b
Nickolay Tsykatunov <ntsykatunov@ozon.ru>
2024-11-11 13:57:13 +0300

    another one changes

--- tests/test_class_bun.py
+++ tests/test_class_bun.py
@@ -7,11 +7,18 @@ class TestBun:
         assert bun.name == "test bun"
         assert bun.price == 100
 
-    @pytest.mark.parametrize("name, price, expected_name, expected_price", [
-        ("white bun", 200, "white bun", 200),
-        ("black bun", 150, "black bun", 150)
+    @pytest.mark.parametrize("name, price, expected_name", [
+        ("white bun", 200, "white bun"),
+        ("black bun", 150, "black bun")
     ])
-    def test_bun_getters(self, name, price, expected_name, expected_price):
+    def test_get_name(self, name, price, expected_name):
         bun = Bun(name, price)
         assert bun.get_name() == expected_name
+
+    @pytest.mark.parametrize("name, price, expected_price", [
+        ("white bun", 200, 200),
+        ("black bun", 150, 150)
+    ])
+    def test_get_price(self, name, price, expected_price):
+        bun = Bun(name, price)
         assert bun.get_price() == expected_price
